c0569dbc1bcd4d08c970e3fffafd7281a2dba3b9
Harsain Kapoor <harsain.kapoor@versent.com.au>
2018-07-13 08:55:31 +1000

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+var _ = require("lodash");
+var schemaValidator = require("ajv");
+
+fs.readFile("./db.json", "utf8", (err, data) => {
+  if (err) {
+    throw new Error("invalid db JSON");
+  }
+
+  data = JSON.parse(data);
+  validateDBSchema(data);
+  entities = Object.keys(data);
+  entities.forEach(element => {
+    router.get("/" + element, function(req, res) {
+      res.send(data[element]);
+    });
+
+    router.get("/" + element + "/:id", function(req, res) {
+      response = _.find(data[element], { id: parseInt(req.params.id) });
+      if (response === undefined) {
+        res.render("error", {
+          error: {
+            status: 400,
+            stack: element + " with ID: " + req.params.id + " not found"
+          }
+        });
+      } else {
+        res.send(response);
+      }
+    });
+  });
+});
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  var routeArr = [];
+  router.stack.forEach(route => {
+    methods = [];
+    for (const method in route.route.methods) {
+      if (method === "_ all") continue;
+      methods.push(method.toUpperCase());
+    }
+    routeArr.push({ path: route.route.path, methods: methods });
+  });
+
+  res.render("index", { title: "APIE", routes: routeArr });
+});
+
+function validateDBSchema(data) {
+  fs.readFile("./schema.json", "utf8", (err, schemaString) => {
+    if (err) {
+      throw new Error("invalid schema file");
+    }
+    var ajv = new schemaValidator();
+    var valid = ajv.validate(JSON.parse(schemaString), data);
+    if (!valid) {
+      throw new Error(ajv.errorsText());
+    }
+  });
+}
+
+module.exports = router;
